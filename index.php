352ac85ba8fd0d830c370d65abdc293e037fcd02
Folen0909 <davidturkalj@gmail.com>
2021-06-15 01:07:02 +0200

    Updated php file

--- index.php
+++ index.php
@@ -13,34 +13,19 @@
     <div id="color-picker">
         <ul>
             <li>
-                <label for="colorQ">Bind color to key 'Q':</label>
-                <input type="color" id="colorQ" name="colorQ" value="#FF0000">
-            </li>
-            <li>
-                <label for="colorW">Bind color to key 'W':</label>
-                <input type="color" id="colorW" name="colorW" value="#00FF00">
-            </li>
-            <li>
-                <label for="colorE">Bind color to key 'E':</label>
-                <input type="color" id="colorE" name="colorE" value="#0000FF">
-            </li>
-            <li>
-                <label for="colorR">Bind color to key 'R':</label>
-                <input type="color" id="colorR" name="colorR" value="#FFFFFF">
-            </li>
-            <li>
-                <label for="colorT">Bind color to key 'T':</label>
-                <input type="color" id="colorT" name="colorT" value="#000000">
+                <label for="drawingColor">Pick drawing color:</label>
+                <input type="color" id="drawingColor" name="drawingColor" 
+                    onchange="drawingColorChange()" value="#000000">
             </li>
             <li>
                 <label for="backgroundColor">Background color: </label>
-                <input type="color" id="backgroundColor" name="backgroundColor" onchange="backgroundColorChange()"
-                    value="#D3D3D3">
+                <input type="color" id="backgroundColor" name="backgroundColor" 
+                    onchange="backgroundColorChange()" value="#D3D3D3">
             </li>
         </ul>
-        <button id="replayButton" type="button" onclick="replay()">Replay</button>
-        <label for="selectedColor">Current selected color:</label>
-        <input disabled type="color" id="selectedColor" name="selectedColor" value="#FFFFFF">
+        <button id="replayButton" type="button" onclick='playFeature("replay")'>Replay</button>
+        <button id="drawButton" type="button" onclick='playFeature("draw")'>Draw Pacman (takes about 2 min)</button>
+        <button id="refreshButton" type="button" onclick='playFeature("refresh")'>Refresh</button>
     </div>
     <div id="dots">
 
